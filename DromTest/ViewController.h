c85f95a7477f23fb748a00f228688ca5723b7911
semeeenov_viktor <vsemenov@asdgroup.pro>
2019-10-04 23:10:01 +0700

    Initial Commit

---
+++ DromTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DromTest
+//
+//  Created by Victor on 04/10/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DromTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DromTest
+//
+//  Created by Victor on 04/10/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
